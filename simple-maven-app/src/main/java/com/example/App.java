4752fdc832d897ec38757fe7f9e70fc71ac837b3
jashwanth7ram <165251278+jashwanth7ram@users.noreply.github.com>
2026-03-02 17:38:16 +0530

    Update App.java

--- simple-maven-app/src/main/java/com/example/App.java
+++ simple-maven-app/src/main/java/com/example/App.java
@@ -1,13 +1,23 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public String getGrade(int a) {
+
+        if (a >= 90 && a <= 100) {
+            return "Grade S";
+        } else if (a >= 80 && a <= 89) {
+            return "Grade A";
+        } else if (a >= 70 && a <= 79) {
+            return "Grade B";
+        } else if (a >= 60 && a <= 69) {
+            return "Grade C";
+        } else if (a >= 50 && a <= 59) {
+            return "Grade D";
+        } else if (a >= 40 && a <= 49) {
+            return "Grade E";
+        } else {
+            return "Grade F";
+        }
     }
 }
