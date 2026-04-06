e1691bf609c4fb1a7e21434a4d5aad97522fed0f
P-jashwanthram <pedelli.jashwanth2023@vitstudent.ac.in>
2026-04-06 10:34:01 +0530

    added a new message

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,7 @@ package com.example;
 
 public class App {
     public double calculateNetSalary(double basic, double hra, double da, double pf, double tax) {
+        // gross salary
         double grossSalary = basic + hra + da;
         double totalReductions = pf + tax;
         double netSalary = grossSalary - totalReductions;
