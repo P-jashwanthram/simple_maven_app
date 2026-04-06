799d05faa7ade88b5ea4a07c36f277d2b8f8f2f0
P-jashwanthram <pedelli.jashwanth2023@vitstudent.ac.in>
2026-04-06 10:18:07 +0530

    Remove getGrade method and add calculateNetSalary method

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,23 +1,10 @@
 package com.example;
 
 public class App {
-
-    public String getGrade(int a) {
-
-        if (a >= 90 && a <= 100) {
-            return "Grade S";
-        } else if (a >= 80 && a <= 89) {
-            return "Grade A";
-        } else if (a >= 70 && a <= 79) {
-            return "Grade B";
-        } else if (a >= 60 && a <= 69) {
-            return "Grade C";
-        } else if (a >= 50 && a <= 59) {
-            return "Grade D";
-        } else if (a >= 40 && a <= 49) {
-            return "Grade E";
-        } else {
-            return "Grade F";
-        }
+    public double calculateNetSalary(double basic, double hra, double da, double pf, double tax) {
+        double grossSalary = basic + hra + da;
+        double totalReductions = pf + tax;
+        double netSalary = grossSalary - totalReductions;
+        return netSalary;
     }
 }
